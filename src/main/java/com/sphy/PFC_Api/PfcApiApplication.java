fbcc9e83e5d92f12c972b75bfa80e2b4e67a722f
sphy <sphy.debugger@gmail.com>
2024-10-14 20:00:27 +0200

    Start project

---
+++ src/main/java/com/sphy/PFC_Api/PfcApiApplication.java
@@ -0,0 +1,13 @@
+package com.sphy.PFC_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sphy/PFC_Api/PfcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sphy.PFC_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
